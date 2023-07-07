18630fa5f7b2f15ce2e4e606d77d71e436789229
Lament93 <122051624+Lament93@users.noreply.github.com>
2023-07-07 15:28:24 +0000

    Update Main2.java
    test

--- src/main/java/ua/hillel/maievskyi/lessons/lesson3/Main2.java
+++ src/main/java/ua/hillel/maievskyi/lessons/lesson3/Main2.java
@@ -3,5 +3,6 @@ package ua.hillel.maievskyi.lessons.lesson3;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hi Git!2");
+        System.out.println("Hi GitHUB");
     }
 }
